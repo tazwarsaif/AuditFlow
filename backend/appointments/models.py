4fd87857276d1343bb8feac5ff182664e7329cca
tazwarsaif <tazwarsaif>
2024-11-13 10:15:44 +0600

    appointmentreschedulerequest models made

--- backend/appointments/models.py
+++ backend/appointments/models.py
@@ -1,2 +1,17 @@
 from django.db import models
+from core.models import User
 
+class AppoinmentRescheduleRequest(models.model):
+    status = (
+        ('Confirmed',"Confirmed"),
+        ('Pending',"Pending"),
+        ('Pending',"Pending"),
+        ('Canceled',"Canceled")
+    )
+    appointment_id = models.ForeignKey(Appointment)
+    sent_to = models.ForeignKey(User,on_delete=models.CASCADE)
+    sent_by = models.ForeignKey(User)
+    suggested_start_time = models.DateTimeField(auto_now_add=False)
+    suggested_end_time = models.DateTimeField(auto_now_add=False)
+    
+    
