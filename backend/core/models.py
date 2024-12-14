22a99b2a3dda310ed5afa12005e22002913aa33e
Rubaba <iamrubaba@gmail.com>
2024-12-14 10:25:59 +0600

    auditors status and location

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,5 +1,8 @@
+from random import choice, choices
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import CharField
 
 
 class User(AbstractUser):
@@ -13,12 +16,19 @@ class User(AbstractUser):
 
 
 class Auditor(models.Model):
+    STATUS = (
+        ("ONLINE", "ONLINE"),
+        ("ON THE WAY", "ON THE WAY"),
+        ("OFFLINE", "OFFLINE")
+    )
     first_name=models.CharField(max_length=255, blank=True, null=True)
     last_name = models.CharField(max_length=255, blank=True, null=True)
     email=models.EmailField()
     phone = models.CharField(max_length=20, blank=True, null=True)
     specializations=models.CharField(max_length=255, blank=True, null=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    status=models.CharField(max_length=20, choices=STATUS, default="OFFLINE")
+    current_location=CharField(max_length=255, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
