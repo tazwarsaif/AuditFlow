3fd2d463eb550144b90c383912d081628b211ec1
tazwarsaif <tazwarsaif>
2025-01-03 16:31:04 +0600

    Fixed merge conflicts

--- backend/core/serializers.py
+++ backend/core/serializers.py
@@ -58,9 +58,3 @@ class LeaveApplicationSerializer(serializers.ModelSerializer):
     def get_applicant(self, obj):
         return obj.sent_by.get_full_name()
 
-
-class PerformanceReportSerializer(serializers.ModelSerializer):
-    class Meta:
-        model= PerformanceReport
-        fields = ['auditor', 'performance_report', 'submitted_at']
-        read_only_fields=['id']
